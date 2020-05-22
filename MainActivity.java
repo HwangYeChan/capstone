7c5cb4f3701cbc5a3034407726ae0a633f721c51
loonatheworld <31719849+HwangYeChan@users.noreply.github.com>
2020-05-22 20:03:11 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -5,7 +5,12 @@ import java.net.*;
 import java.util.HashMap;
 
 import android.app.*;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.ContentValues;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.*;
 import android.util.*;
 import android.view.*;
@@ -17,18 +22,27 @@ public class MainActivity extends TabActivity {
     // 전역변수를 선언한다
     TabHost mTabHost = null;
     String myId, myPWord, myTitle, mySubject, myResult;
+    int cas;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+
+
         mTabHost = getTabHost();          // Tab 만들기
         mTabHost.addTab(mTabHost.newTabSpec("tab_1").setIndicator("서버로 전송").setContent(R.id.page01));
         mTabHost.addTab(mTabHost.newTabSpec("tab_2").setIndicator("서버에서 받음").setContent(R.id.page02));
+
+
+
         findViewById(R.id.button_submit).setOnClickListener(new View.OnClickListener() {
+
             @Override
+
             public void onClick(View v) {
+
                 String url = "http://sitaiyo.iptime.org:8888/listen"; 	//URL
 //                String url="http://10.0.2.2/testhttp/servertest.php";
                 HashMap<String, String> param = new HashMap<String, String>();
@@ -40,10 +54,31 @@ public class MainActivity extends TabActivity {
                 mySubject = ((EditText)(findViewById(R.id.edit_subject))).getText().toString();
 
 
+                File imgFile;
+
+
+                if(myId.equals("chicken")){
+                    cas=1;
+                    imgFile = new  File("/storage/emulated/0/Download/hello.jpg");
+                    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                    param.put("image",BitmapToString(myBitmap));
+                }
+                else if(myId.equals("noimage")){
+                    cas=2;
+                }
+                else{
+                    cas=3;
+                    imgFile = new  File("/storage/emulated/0/DCIM/Camera/IMG_20200521_142359.jpg");
+                    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                    param.put("image",BitmapToString(myBitmap));
+                }
+
                 param.put("id",myId);
                 param.put("pword", myPWord);
                 param.put("title", myTitle);
                 param.put("subject", mySubject);
+
+
 
                 NetworkTask networkTask = new NetworkTask(url, param);
                 networkTask.execute();
@@ -83,10 +118,19 @@ public class MainActivity extends TabActivity {
             // 결과에 따른 UI 수정 등은 여기서 합니다.
             ((TextView)(findViewById(R.id.text_result))).setText(result);
 //            ((TextView)(findViewById(R.id.text_result))).setText(values.toString());
-            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), myId+"!! cas is "+cas+" and "+result, Toast.LENGTH_LONG).show();
         }
     }
 
+
+
+    public static String BitmapToString(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 70, baos);
+        byte[] bytes = baos.toByteArray();
+        String temp = Base64.encodeToString(bytes, Base64.DEFAULT);
+        return temp;
+    }
 
 
 
